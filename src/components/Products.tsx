b0724ad1e89257c299bd366db8cc35adea99e75e
bhupender <b.mehtaa2021@gmail.com>
2025-05-30 01:35:35 +0530

    products.tsx is done

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,126 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
 const Products = () => {
   return (
     <div className="container mx-auto max-w-6xl">
-      <div className="text-warmdark text-center text-5xl font-bold">
+      <div className="text-warmdark mb-10 text-center text-5xl font-bold">
         Our Best Sellers
       </div>
 
-      <div></div>
+      <div className="grid grid-cols-3 gap-6">
+        <Card>
+          <CardHeader>
+            <Image
+              src="/Products/product1.jpg"
+              alt="product1"
+              height={100}
+              width={500}
+            />
+
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="bg-warmdark">Buy Now</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Image
+              src="/Products/product2.jpg"
+              alt="product1"
+              height={100}
+              width={500}
+            />
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="bg-warmdark">Buy Now</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Image
+              src="/Products/product3.jpg"
+              alt="product1"
+              height={100}
+              width={500}
+            />
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="bg-warmdark">Buy Now</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Image
+              src="/Products/product4.jpg"
+              alt="product1"
+              height={100}
+              width={500}
+            />
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="bg-warmdark">Buy Now</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Image
+              src="/Products/product5.jpg"
+              alt="product1"
+              height={100}
+              width={500}
+            />
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="bg-warmdark">Buy Now</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Image
+              src="/Products/product6.jpg"
+              alt="product1"
+              height={100}
+              width={500}
+            />
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="bg-warmdark">Buy Now</Button>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
