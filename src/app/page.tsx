8b651378d25e423c054fdb50fd4bd2718d72650a
bhupender <b.mehtaa2021@gmail.com>
2025-05-30 01:27:10 +0530

    i added the photos 6 photos here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import Products from "@/components/Products";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <Products />
     </div>
   );
 }
---
+++ src/components/Products.tsx
@@ -0,0 +1,13 @@
+const Products = () => {
+  return (
+    <div className="container mx-auto max-w-6xl">
+      <div className="text-warmdark text-center text-5xl font-bold">
+        Our Best Sellers
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Products;
