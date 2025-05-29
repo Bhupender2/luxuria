36b66e87aca10516d48ff1f71d569fc0be77391a
bhupender <b.mehtaa2021@gmail.com>
2025-05-30 00:55:41 +0530

    button is added and hero section is created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
+    <div>
+      <HeroSection />
     </div>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
 const HeroSection = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto flex max-w-7xl justify-between">
+      <div className="text-warmdark text-5xl font-bold">
+        <div>Curated Luxury</div>
+        <div>Gifts , Delivered</div>
+        <div>with Love</div>
+      </div>
+      <div>
+        <Image
+          src="/luxuriabg.png"
+          alt="background_img"
+          height={300}
+          width={400}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
