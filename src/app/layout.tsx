2584e2c021cecbfe9fcc5b219f22222655c335c0
bhupender <b.mehtaa2021@gmail.com>
2025-05-30 02:39:26 +0530

    footer is added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const poppins = Montserrat({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t-2 border-gray-200 p-4 shadow-2xl">
+      <div className="mx-auto flex min-h-20 max-w-7xl justify-between">
+        <div>@copyright Luxuria Pvt Ltd, All Right Reserved</div>
+        <div className="flex gap-2">
+          <div>Privacy Policy</div>
+          <div>Terms of Services</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
