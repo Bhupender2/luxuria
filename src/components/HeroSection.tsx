abbb93749fcdd7e011e5418814ac846d1f4e6faa
bhupender <b.mehtaa2021@gmail.com>
2025-05-30 01:00:40 +0530

    hero section is almost completed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 const HeroSection = () => {
   return (
-    <div className="container mx-auto flex max-w-7xl justify-between">
-      <div className="text-warmdark text-5xl font-bold">
-        <div>Curated Luxury</div>
-        <div>Gifts , Delivered</div>
-        <div>with Love</div>
+    <div className="container mx-auto flex max-w-6xl justify-between">
+      <div className="flex items-center justify-center">
+        <div className="text-warmdark text-5xl font-bold">
+          <div>Curated Luxury</div>
+          <div>Gifts , Delivered</div>
+          <div>with Love</div>
+          <Button className="bg-warmdark">Shop Now</Button>
+        </div>
       </div>
       <div>
         <Image
           src="/luxuriabg.png"
           alt="background_img"
-          height={300}
+          height={100}
           width={400}
         />
       </div>
